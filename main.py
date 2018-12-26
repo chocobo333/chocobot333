03b5acdaf7534af580223cd1bd5b62abe3019501
chocobo333 <chocobo1355@gmail.com>
2018-12-27 00:57:49 +0900

    hello world

---
+++ main.py
@@ -0,0 +1,23 @@
+
+import settings
+import discord
+import asyncio
+
+DISCORD_TOKEN = settings.DISCORD_TOKEN
+print(DISCORD_TOKEN)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("logged in")
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("/neko"):
+        reply = "awewewewe"
+        await client.send_message(message.channel,reply)
+
+
+
+client.run(DISCORD_TOKEN)
