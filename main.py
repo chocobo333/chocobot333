565b3420e7f9d5732d2d7e0b238d3826bd0961f4
chocobo333 <chocobo1355@gmail.com>
2018-12-27 14:59:08 +0900

    upgrade discord.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import settings
 import discord
 import asyncio
+from discord.ext import commands
 
 DISCORD_TOKEN = settings.DISCORD_TOKEN
 
@@ -12,11 +13,9 @@ async def on_ready():
     print("logged in")
 
 @client.event
-async def on_message(message):
+async def on_message(message:discord.message):
     if message.content.startswith("/neko"):
         reply = "awewewewe"
         await client.send_message(message.channel,reply)
-
-
 
 client.run(DISCORD_TOKEN)
