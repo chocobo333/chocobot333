78725250fb069fe7a104fe75c7da5aaca53cf628
chocobo333 <chocobo1355@gmail.com>
2018-12-28 17:13:37 +0900

    make it better

--- cogs/common.py
+++ cogs/common.py
@@ -6,12 +6,8 @@ class common:
 
     @commands.command(aliases=["きる"])
     async def kill(self,ctx):
-        await ctx.("logged out")
+        await ctx.send("logged out")
         await self.bot.logout()
-
-    @commands.command()
-    async def test(self,ctx):
-        await ctx.send("testestestest")
 
 def setup(bot):
     bot.add_cog(common(bot))
--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import asyncio
 import discord
 from discord.ext import commands
 from traceback import print_exc
+import re
 
 DISCORD_TOKEN = settings.DISCORD_TOKEN
 
 extentions = settings.extentions
+
+url_only_channel_id = 528091405273268237
+
+url_pattern = "(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)"
 
 class BotClient(commands.Bot):
     def __init__(self,**kwargs):
@@ -19,16 +24,27 @@ class BotClient(commands.Bot):
             except Exception:
                 print_exc()
 
+        self.unload_extension("cogs.VoiceChat")
+
     async def on_ready(self):
         print("logged in")
         print(self.user.name)
         print(self.user.id)
 
-    async def on_message(self,message):
+    async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
 
+        if message.channel.id == url_only_channel_id:
+            match = re.match(url_pattern,message.content)
+            if not match:
+                await message.delete()
+                msg = await message.channel.send("In this channel, you are allowed to send only url.")
+                await asyncio.sleep(3)
+                await msg.delete()
+                
         await self.process_commands(message)
+    
 
     # async def on_command_error(self,ctx,error):
     #     await ctx.send(str(error))
